cc2e18eca259eecac9346579ea0c040ea2e6937a
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2012-05-12 10:59:26 +0200

    Removed example code from gh-pages branch, merged buster configs.

--- app.js
+++
@@ -1,4 +0,0 @@
-var connect = require('connect');
-var app = connect();
-app.use(connect.static(__dirname));
-app.listen(3000);
--- example/test/phantom/server.js
+++
@@ -1,8 +0,0 @@
-var busterTestCli = require("buster-test-cli");
-
-exports.run = function (fn) {
-
-  var server = busterTestCli.cli.server.create();
-  server.run();
-  fn(server);
-};
